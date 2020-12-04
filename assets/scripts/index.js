5ec89e6535d9b9271a4866433c56fd6e78035e4b
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-12-03 23:39:14 -0300

    Getting current geolocation

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,10 +2,17 @@ import functions from './functions/index.js';
 
 let currentCity = '';
 
-const loadWeatherData = async (cityName) => {
+const loadWeatherData = async (cityName, coordinates = null) => {
   cityName = cityName.trim();
 
-  if (cityName === currentCity) return;
+  if (!coordinates && cityName === currentCity) return;
+
+  const coordsParams = coordinates
+    ? {
+        lat: coordinates.latitude,
+        lon: coordinates.longitude,
+      }
+    : {};
 
   try {
     const openWeatherMapUrl = new URL(
@@ -16,6 +23,7 @@ const loadWeatherData = async (cityName) => {
       appid: 'ae323e75c00e8065ffb70d2e6ba56373',
       lang: 'pt_br',
       units: 'metric',
+      ...coordsParams,
     };
 
     Object.keys(queryParams).forEach((key) =>
@@ -46,6 +54,34 @@ const handleKeyUp = async ({ target: { value: cityName } }) => {
   });
 };
 
-document
-  .querySelector('input[name=city]')
-  .addEventListener('keyup', functions.debounceEvent(handleKeyUp, 800));
+const handleFailureToGetGeoPermission = (e) => {
+  alert(
+    'talvez voce queira conseder a permissao de geolocalizacao para pesquisar ne ',
+  );
+};
+
+const handleSuccessToGetGeoPermission = async (geolocationPosition) => {
+  const weatherData = await loadWeatherData('', geolocationPosition.coords);
+
+  if (!weatherData) return;
+
+  functions.fillFields({
+    weatherData,
+    generalContainer: document.querySelector('.weather-short-info'),
+    minMaxContainer: document.querySelector('.maximum-minimum > div'),
+    detailedContainer: document.querySelector('.general-info'),
+  });
+};
+
+window.onload = function () {
+  document
+    .querySelector('input[name=city]')
+    .addEventListener('keyup', functions.debounceEvent(handleKeyUp, 800));
+
+  if (this.navigator.geolocation) {
+    window.navigator.geolocation.getCurrentPosition(
+      handleSuccessToGetGeoPermission,
+      handleFailureToGetGeoPermission,
+    );
+  }
+};
